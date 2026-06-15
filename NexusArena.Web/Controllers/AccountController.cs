1c15286ffce09a13a09a09526048fc4366b275b0
SahilMirza1779 <sahilmirza29365@gmail.com>
2026-06-15 11:25:42 +0530

    Created AccountCOntroller inn MVC and also Created Login.cshtml in View -> Account -> Login.cshtml

---
+++ NexusArena.Web/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NexusArena.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            string userRole = GetRoleFromApiDummy(email, password);
+
+            if (userRole == "Invalid")
+            {
+                ViewBag.Error = "Invalid Email or Password!";
+                return View();
+            }
+
+            switch (userRole)
+            {
+                case "SuperAdmin":
+                    return RedirectToAction("Index", "SuperAdminDashboard");
+
+                case "Owner":
+                    return RedirectToAction("Index", "OwnerDashboard");
+
+                case "Receptionist":
+                    return RedirectToAction("Index", "ReceptionistDashboard");
+
+                case "User":
+                    return RedirectToAction("Index", "Home");
+
+                default:
+                    return RedirectToAction("Login");
+            }
+        }
+
+        private string GetRoleFromApiDummy(string email, string password)
+        {
+            if (email == "admin@nexus.com" && password == "123") return "SuperAdmin";
+            if (email == "owner@nexus.com" && password == "123") return "Owner";
+            if (email == "reception@nexus.com" && password == "123") return "Receptionist";
+            if (email == "player@nexus.com" && password == "123") return "User";
+
+            return "Invalid";
+        }
+    }
+}
