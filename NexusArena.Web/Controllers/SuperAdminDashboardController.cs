8d253d0863f8bc2acfbd36af53062d6466ecf3cb
SahilMirza1779 <sahilmirza29365@gmail.com>
2026-06-15 16:06:23 +0530

    I changed some things in Login page and connect Web Api to MVC project

--- NexusArena.Web/Controllers/SuperAdminDashboardController.cs
+++ NexusArena.Web/Controllers/SuperAdminDashboardController.cs
@@ -1,12 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticAssets;
+using System.Text.Json;
+using NexusArena.Web.Models;
 
 namespace NexusArena.Web.Controllers
 {
     public class SuperAdminDashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SuperAdminDashboardController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            string apiUrl = "http://localhost:5092/api/Dashboard/Stats";
+            var dashboardData = new DashboardStatsViewModel();
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonData = await response.Content.ReadAsStringAsync();
+                    dashboardData = JsonSerializer.Deserialize<DashboardStatsViewModel>(jsonData, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                }
+                else
+                {
+                    ViewBag.Error = "Failed to fetch data from the API. Status code: " + response.StatusCode;
+                    SetDefaultStats(dashboardData);
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "The API is completely inaccessible. Please check if API project is running.";
+                SetDefaultStats(dashboardData);
+            }
+            return View(dashboardData);
+        }
+
+        private void SetDefaultStats(DashboardStatsViewModel model)
+        {
+            model.TotalPlayers = 0;
+            model.RegisteredOwners = 0;
+            model.ActiveArenas = 0;
+            model.PlatformRevenue = "₹0";
         }
     }
 }
---
+++ NexusArena.Web/Models/DashboardStatsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NexusArena.Web.Models
+{
+    public class DashboardStatsViewModel
+    {
+        public int TotalPlayers { get; set; }
+        public int RegisteredOwners { get; set; }
+        public int ActiveArenas { get; set; }
+        public string? PlatformRevenue { get; set; }
+    }
+}
