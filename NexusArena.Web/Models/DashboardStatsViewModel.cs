dd6add43aac54b5b7b4dde864303f3fc216fe491
SahilMirza1779 <sahilmirza29365@gmail.com>
2026-06-16 12:06:46 +0530

    In WEB API project I Impliment DashboardController for APIs fetch On other hand in MVC project I made some changes in SuperAdminController and many more

--- NexusArena.Web/Models/DashboardStatsViewModel.cs
+++ NexusArena.Web/Models/DashboardStatsViewModel.cs
@@ -4,7 +4,9 @@
     {
         public int TotalPlayers { get; set; }
         public int RegisteredOwners { get; set; }
+        public int TotalReceptionists { get; set; }
         public int ActiveArenas { get; set; }
         public string? PlatformRevenue { get; set; }
+        public List<PendingArenaViewModel> PendingApprovals { get; set; } = new List<PendingArenaViewModel>();
     }
 }
---
+++ NexusArena.Web/Models/PendingArenaViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NexusArena.Web.Models
+{
+    public class PendingArenaViewModel
+    {
+        public int Id { get; set; }
+        public string ArenaName { get; set; }
+        public string OwnerName { get; set; }
+        public string Category { get; set; }
+        public string Status { get; set; }
+    }
+}
